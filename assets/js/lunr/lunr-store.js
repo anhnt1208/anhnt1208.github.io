ca02b635b0c164a848e32c9c863c0b19ad3bc275
anhnt1208 <anhnt1208@users.noreply.github.com>
2024-02-26 02:11:24 +0000

    jekyll build from Action 816310ada9fc5cc2e57913963bc0ea61c3d48a16

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "How to Optimize MySQL Queries for Speed and Performance",
+        "excerpt":"Tip #1: Using Index Index All Columns Used in where, order by, and group by Clauses An index is also very useful when it comes to sorting records. Check the explain statement of query from the table with 500 rows without index mysql&gt; explain select customer_id, customer_name from customers where...","categories": [],
+        "tags": ["performance","optimize","database","mysql"],
+        "url": "/How-to-optimize-mysql-queries/",
+        "teaser": null
+      },{
+        "title": "IP Addressing - Introduction and Classful Addressing",
+        "excerpt":"A classful network is a network addressing architecture used in the Internet from 1981 until the introduction of Classless Inter-Domain Routing (CIDR) in 1993. The method divides the IP address space for Internet Protocol version 4 (IPv4) into five address classes based on the leading four address bits. IP Addressing...","categories": [],
+        "tags": ["infrastructure"],
+        "url": "/IP-Addressing-Introduction-and-Classful-Addressing/",
+        "teaser": null
+      },{
+        "title": "Systemd vs /etc/init.d",
+        "excerpt":"The init daemon is going to be replaced with daemon systemd on some of the Linux Distributions, while a lot of them have already implemented it. This is/will be creating a huge gap between traditional Unix/Linux Guard and New Linux Guard – programmers and System Admins. What is Init ?...","categories": [],
+        "tags": ["unix","ubuntu"],
+        "url": "/systemd-vs-init/",
+        "teaser": null
+      },{
+        "title": "Create Slack Bot with AWS Serverless",
+        "excerpt":"A bot is a nifty way to run code and automate tasks. In Slack, a bot is controlled programmatically via a bot user token that can access one or more of Slack’s APIs. We will create simple bot that receive and process some simple task by entering command into channel...","categories": [],
+        "tags": ["AWS","Slack","Serverless","API Gateway","Lambda"],
+        "url": "/create-slack-bot-with-aws-serverless/",
+        "teaser": null
+      },{
+        "title": "Include diagrams into Markdown with Mermaid",
+        "excerpt":"Mermaid is a JavaScript based diagramming and charting tool that takes Markdown-inspired text definitions and creates diagrams dynamically in the browser. Maintained by Knut Sveidqvist, it supports a bunch of different common diagram types for software projects, including flowcharts, UML, Git graphs, user journey diagrams, and even the dreaded Gantt...","categories": [],
+        "tags": ["Docs as Code","Markdown"],
+        "url": "/include-diagram-into-md-with-mermaid/",
+        "teaser": null
+      },{
+        "title": "API Hub - Part 01",
+        "excerpt":"Background An API hub serves as a centralized repository, allowing several projects can register and integrate all APIs seamlessly, utilizing a single key. Internal API Hub: serve API for companies projects External API Hub: Customers can register and use APIs for their projects, Project goals Reduce development cycles Creating stickiness...","categories": [],
+        "tags": ["Architecture","MicroService"],
+        "url": "/api-hub-architecture/",
+        "teaser": null
+      }]
